025af2a2a78e0d403ff303017a6f2b28e9c652f1
Mayconfuzita86 <mayconfuzita@gmail.com>
2023-10-15 14:20:36 -0300

    [ROCKETSEAT] Treinamento Java - Aulas 01 e 02

---
+++ src/main/java/com/mayconfuzita/todolisttwo/TodolisttwoApplication.java
@@ -0,0 +1,13 @@
+package com.mayconfuzita.todolisttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolisttwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolisttwoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mayconfuzita/todolisttwo/user/UserController.java
@@ -0,0 +1,32 @@
+package com.mayconfuzita.todolisttwo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired // irá gerenciar todo ciclo de vida da inteface
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel){
+
+       var user = this.userRepository.findByUsername(userModel.getUsername());
+
+       if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+       }
+
+       var userCreated = this.userRepository.save(userModel);
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+    
+}
